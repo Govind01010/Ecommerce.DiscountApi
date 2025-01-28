78d4eccaa8f559b456d819e1107384a916830156
Govind Sharma <iamgovinda100@gmail.com>
2025-01-28 23:14:51 +0530

    Created Discount Database and Discount Table

--- Discount/Discount.Infrastructure.Data/AppDbContext.cs
+++ Discount/Discount.Infrastructure.Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace Discount.Infrastructure.Data
 {
@@ -9,5 +10,24 @@ namespace Discount.Infrastructure.Data
         }
 
         public DbSet<Domain.Entities.Discount> Discounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(ConvertToSnakeCase(entity.GetTableName()));
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(ConvertToSnakeCase(property.Name));
+                }
+            }
+        }
+
+        private string ConvertToSnakeCase(string name)
+        {
+            return Regex.Replace(name, "([a-z0-9])([A-Z])", "$1_$2").ToLower();
+        }
     }
 }
---
+++ Discount/Discount.Infrastructure.Data/Migrations/20250127165111_initial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discount.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "discounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    percentage = table.Column<int>(type: "int", nullable: false),
+                    minimum_amount = table.Column<int>(type: "int", nullable: false),
+                    is_active = table.Column<bool>(type: "bit", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    created_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    updated_by = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    deleted_by = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_discounts", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "discounts");
+        }
+    }
+}
