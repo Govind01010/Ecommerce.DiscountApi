aaa61e36a77bcd2c6b90dcfb572461b2ac23dd8f
Govind Sharma <iamgovinda100@gmail.com>
2025-02-23 22:47:51 +0530

    Revert "Switch to common ResponseDto and Added Try Catch"
    This reverts commit 5692bda81ff8592e43304f1adfd0bdee312b0b46.

--- Discount/Discount.Domain/Dtos/ResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Discount.Domain.Dtos
-{
-    public class ResponseDto
-    {
-        public object? result { get; set; }
-        public bool isSuccess { get; set; } = true;
-        public string message { get; set; } = "";
-    }
-}
--- Discount/Discount/Controller/DiscountController.cs
+++ Discount/Discount/Controller/DiscountController.cs
@@ -22,28 +22,11 @@ namespace Discount.Controller
         [HttpGet("get-coupon-by-id/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetDiscountCouponById([FromRoute] int id)
+        public async Task<IActionResult> GetDiscountCouponById([FromRoute] int discountCouponId)
         {
-            try
-            {
-                var query = new GetDiscountCouponByIdQuery(id);
-                var response = await _mediator.Send(query);
-                if (response.IsSuccess)
-                {
-                    var result = new ResponseDto
-                    {
-                        isSuccess = true,
-                        message = "Successfully Fetched All Discount Coupons",
-                        result = response.Value
-                    };
-                    return Ok(result);
-                }
-                return NotFound(new ResponseDto { isSuccess = false, message = "Discount coupon not found", result = null });
-            }
-            catch (Exception ex)
-            {
-                return NotFound(new ResponseDto { isSuccess = false, message = ex.Message, result = null });
-            }
+            var query = new GetDiscountCouponByIdQuery(discountCouponId);
+            var response = await _mediator.Send(query);
+            return Ok(response);
         }
 
         [HttpGet("get-all-coupons")]
@@ -51,92 +34,36 @@ namespace Discount.Controller
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetAllDiscountCoupons()
         {
-            try
-            {
-                var query = new GetDiscountCouponsQuery();
-                var response = await _mediator.Send(query);
-                if (response.IsSuccess)
-                {
-                    var result = new ResponseDto
-                    {
-                        isSuccess = true,
-                        message = "Successfully Fetched All Discount Coupons",
-                        result = response.Value
-                    };
-                    return Ok(result);
-                }
-                return NotFound(new ResponseDto { isSuccess = false, message = "Discount coupon not found", result = null });
-            }
-            catch (Exception ex)
-            {
-                return NotFound(new ResponseDto { isSuccess = false, message = ex.Message, result = null });
-            }
+            var query = new GetDiscountCouponsQuery();
+            var response = await _mediator.Send(query);
+            return Ok(response);
         }
 
         [HttpPost("create-coupon")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> CreateDiscount([FromBody] Domain.Dtos.DiscountDto discountDto)
         {
-            try
-            {
-                var request = new CreateDiscountCouponCommand(discountDto);
-                var response = await _mediator.Send(request);
-                if (response.IsSuccess)
-                {
-                    var result = new ResponseDto
-                    {
-                        isSuccess = true,
-                        message = "Successfully Create Discount Coupons"
-                    };
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ResponseDto { isSuccess = false, message = ex.Message, result = null });
-            }
+            var request = new CreateDiscountCouponCommand(discountDto);
+            var response = await _mediator.Send(request);
+            return Ok(response);
         }
 
         [HttpPut("update-coupon")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> UpdateDiscount([FromBody] DiscountDto coupon)
         {
-            try
-            {
-                var request = new UpdateDiscountCouponCommand(coupon);
-                var response = await _mediator.Send(request);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ResponseDto { isSuccess = false, message = ex.Message, result = null });
-            }
+            var request = new UpdateDiscountCouponCommand(coupon);
+            var response = await _mediator.Send(request);
+            return Ok(response);
         }
 
         [HttpDelete("delete-coupon/{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> DeleteDiscount([FromRoute] int id)
+        public async Task<IActionResult> DeleteDiscount([FromRoute]int id)
         {
-            try
-            {
-                var request = new DeleteDiscountCouponCommand(id);
-                var response = await _mediator.Send(request);
-                if (response.IsSuccess)
-                {
-                    var result = new ResponseDto
-                    {
-                        isSuccess = true,
-                        message = "Successfully deleted discount coupon",
-                        result = null
-                    };
-                    return Ok(result);
-                }
-                return BadRequest(new ResponseDto { isSuccess = false, message = response.ErrorMessage, result = null });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ResponseDto { isSuccess = false, message = ex.Message, result = null });
-            }
+            var request = new DeleteDiscountCouponCommand(id);
+            var response = await _mediator.Send(request);
+            return Ok();
         }
     }
 }
